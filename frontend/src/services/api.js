341e891b910897736d218af29f3d734aee02c56f
Albert Dima <albertdima9215@gmail.com>
2026-07-27 14:36:23 +0000

    Correction de l'URL de l'API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://projetagri.onrender.com/api/...",
+  baseURL: "https://projetagri.onrender.com/api",
 });
 
 export default api;
