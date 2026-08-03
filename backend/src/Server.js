883b25289a1387800c7b72ee95c4d2193dd7c97f
Albert Dima <albertdima9215@gmail.com>
2026-08-03 22:45:48 +0000

    Modification de Server

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -11,14 +11,21 @@ app.listen(PORT, () => {
     console.log(`Serveur démarré sur le port ${PORT}`);
 });*/
 
-const express = require("express");
+require("dotenv").config();
+const app = require("./App");
+
 const http = require("http");
 const { Server } = require("socket.io");
+const connectDB = require("./config/database");
 
-const app = express();
+const PORT = process.env.PORT || 5000;
 
+connectDB();
+
+// Créer le serveur HTTP à partir de l'app Express existante
 const server = http.createServer(app);
 
+// Socket.IO
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -26,7 +33,7 @@ const io = new Server(server, {
   },
 });
 
-// utilisateurs connectés
+// Utilisateurs connectés
 const users = {};
 
 io.on("connection", (socket) => {
@@ -34,7 +41,7 @@ io.on("connection", (socket) => {
 
   socket.on("register", (userId) => {
     users[userId] = socket.id;
-    console.log("Register", userId);
+    console.log("Utilisateur enregistré :", userId);
   });
 
   socket.on("disconnect", () => {
@@ -48,13 +55,11 @@ io.on("connection", (socket) => {
   });
 });
 
+// Rendre io accessible dans les contrôleurs
 app.set("io", io);
 app.set("users", users);
 
-// ... tes routes ici
-
-const PORT = process.env.PORT || 5000;
-
+// Démarrer le serveur
 server.listen(PORT, () => {
-  console.log(`Serveur lancé sur ${PORT}`);
+  console.log(`Serveur démarré sur le port ${PORT}`);
 });
